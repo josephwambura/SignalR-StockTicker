8a18ca3eca746b0beb9bfdd3af52cf59b98e49af
Joseph Wambura <joseph.wambura@pycs.co.ke>
2021-11-12 10:02:52 +0300

    ++ Owin fix

---
+++ SignalR.StockTicker/SignalR.StockTicker.WebApp/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalR.StockTicker.WebApp.Startup), "Configuration")]
+
+namespace SignalR.StockTicker.WebApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            Microsoft.AspNet.SignalR.StockTicker.WebApp.Startup.ConfigureSignalR(app);
+        }
+    }
+}
